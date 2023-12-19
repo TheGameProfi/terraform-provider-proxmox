fcac1d47506c3f80fc66bb329d891ddb77611074
Marc Baumgartner <marc.baumgartner@virtualminds.com>
2023-12-19 15:21:02 +0100

    fix wrong error

--- proxmox/resource_storage_iso.go
+++ proxmox/resource_storage_iso.go
@@ -102,6 +102,9 @@ func _downloadFile(url string, file *os.File) error {
 	}
 	defer resp.Body.Close()
 	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to Copy: %v", err)
+	}
 	return nil
 }
 
